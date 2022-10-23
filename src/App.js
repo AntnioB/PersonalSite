d21ddb5a3dde40f5900e480b7e5a6069cd99fcb9
Antonio <a.brejo@campus.fct.unl.pt>
2022-10-24 00:33:32 +0100

    Background Movement Achieved

--- src/App.js
+++ src/App.js
@@ -11,10 +11,11 @@ function App() {
 class Link extends React.Component{
   render() {
     return <a 
-    class="menu-item"
+    className="menu-item"
     href={this.props.href}
     target="_blank"
     rel="noopener noreferrer"
+    {...this.props}
     >
       {this.props.name}
     </a>;
@@ -22,15 +23,24 @@ class Link extends React.Component{
 }
 
 class Menu extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      pos: '0% 0%',
+    };
+  }
+  handleHover(x){
+    this.setState({pos: x});
+  }
   render(){
     return(<div id="menu">
     <div id="menu-items">
-      <Link href="https://github.com/AntnioB" name="GitHub"></Link>
-      <Link href="https://www.linkedin.com/in/antonio-brejo/" name="LinkedIn"></Link>
-      <Link href="https://twitter.com/the_R3set" name="Twitter"></Link>
-      <Link href="/" name="GitHub"></Link>
+      <Link href="https://github.com/AntnioB" name="GitHub" onMouseOver={() => this.handleHover('0% -25%')}></Link>
+      <Link href="https://www.linkedin.com/in/antonio-brejo/" name="LinkedIn" onMouseOver={() => this.handleHover('0% -50%')}></Link>
+      <Link href="https://twitter.com/the_R3set" name="Twitter" onMouseOver={() => this.handleHover('0% -75%')}></Link>
+      <Link href="/" name="CV" onMouseOver={() => this.handleHover('0% -100%')}></Link>
     </div>
-    <div id="menu-background-pattern"></div>
+    <div id="menu-background-pattern" style={{backgroundPosition: this.state.pos}}></div>
   </div>);
   }
 }
